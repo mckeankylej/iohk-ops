03ddf98f2e25a720c3871d2b6e77f3120f868102
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-08-04 14:55:54 -0700

    DON'T MERGE: try using older rev of stack2nix to fix Hydra builds
    ./pkgs/generate.sh ends up producing the same nix expression, so this
    is solely for documentation purposes while we diagnose the fault.

--- pkgs/stack2nix.nix
+++ pkgs/stack2nix.nix
@@ -1,22 +1,21 @@
 { mkDerivation, async, base, bytestring, Cabal, containers
 , data-fix, directory, fetchgit, filepath, Glob, hnix
-, monad-parallel, optparse-applicative, path, process
-, SafeSemaphore, stack, stdenv, temporary, text, yaml
+, monad-parallel, optparse-applicative, process, SafeSemaphore
+, stdenv, temporary, text, yaml
 }:
 mkDerivation {
   pname = "stack2nix";
   version = "0.1.3.0";
   src = fetchgit {
     url = "https://github.com/input-output-hk/stack2nix.git";
-    sha256 = "1sdrsqc0xz7xn4knf47qaj3xxs9jns8izcqnx6cn84gpv76hhiqf";
-    rev = "05420f3efb38834700a3da8a5245fecc50b01139";
+    sha256 = "0cc3w5bhazllj2g9fw6fmhya71lklplkqlvyl0jm4qm9rmq3280m";
+    rev = "b26d16d1be42e30083a29fd593daba535d44d736";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     async base bytestring Cabal containers data-fix directory filepath
-    Glob hnix monad-parallel path process SafeSemaphore stack temporary
-    text yaml
+    Glob hnix monad-parallel process SafeSemaphore temporary text yaml
   ];
   executableHaskellDepends = [ base Cabal optparse-applicative ];
   doCheck = false;
