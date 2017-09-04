f9197ab17b84cc8f6e8ae7691fdd36a472c3f3a4
Cardano Staging <staging@cardano>
2017-09-04 08:07:52 +0000

    Update aux scripts

--- redeploy-iwht-db.sh
+++ redeploy-iwht-db.sh
@@ -3,6 +3,7 @@
 
 dump=$1
 delay=$2
+cluster=$3
 
 if [[ ! -f $dump.tgz ]]; then
    echo "No $dump.tgz"
@@ -14,10 +15,15 @@ if [[ $delay == "" ]]; then
    exit 1
 fi
 
+if [[ $cluster == "" ]]; then
+   echo "Provide cluster"
+   exit 1
+fi
+
 io="/nix/store/604hqdwlw82pn19s5aszmxjkrjfadgqn-ghc-8.0.2-with-packages/bin/runhaskell -i/home/staging/iohk/iohk ~/iohk/iohk/iohk-ops.hs"
 
-$io --no-component-check -c csl-1583.yaml stop wipe-journals wipe-node-dbs --confirm
-$io --no-component-check -c csl-1583.yaml ssh "mkdir -p /var/lib/cardano-node"
-for i in a b c d; do nixops scp -d csl-1583 "$i"1 --to $dump.tgz /var/lib/cardano-node/$dump.tgz; done
-$io --no-component-check -c csl-1583.yaml ssh "cd /var/lib/cardano-node; ls; rm -Rf node-db; tar -xzf $dump.tgz; mv $dump node-db; chmod 777 -R node-db"
-$io --no-component-check -c csl-1583.yaml deploy --bump-system-start-held-by $delay
+$io --no-component-check -c $cluster.yaml stop wipe-journals wipe-node-dbs --confirm
+$io --no-component-check -c $cluster.yaml ssh "mkdir -p /var/lib/cardano-node"
+for i in a b c d; do nixops scp -d $cluster "$i"1 --to $dump.tgz /var/lib/cardano-node/$dump.tgz; done
+$io --no-component-check -c $cluster.yaml ssh "cd /var/lib/cardano-node; ls; rm -Rf node-db; tar -xzf $dump.tgz; mv $dump node-db; chmod 777 -R node-db"
+$io --no-component-check -c $cluster.yaml deploy --bump-system-start-held-by $delay
--- retrieve-logs.sh
+++ retrieve-logs.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+cluster=$1
+
+if [[ $cluster == "" ]]; then
+   echo "Provide cluster"
+   exit 1
+fi
+
 io="/nix/store/604hqdwlw82pn19s5aszmxjkrjfadgqn-ghc-8.0.2-with-packages/bin/runhaskell -i/home/staging/iohk/iohk ~/iohk/iohk/iohk-ops.hs"
 
-journaltgz=`$io --no-component-check -c csl-1583.yaml get-journals | grep 'Packing journals into' | sed 's/Packing journals into //g'`
+journaltgz=`$io --no-component-check -c $cluster.yaml get-journals | grep 'Packing journals into' | sed 's/Packing journals into //g'`
 
 journal=${journaltgz:0:-4}
 
